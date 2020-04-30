7b7e73b0d82e31af10eebc92602c10794c59134f
Roynecro <Roynecro97@users.noreply.github.com>
2020-04-30 12:08:57 +0300

    Added support for anonymous namespaces.

--- pyheaders/cpp/scope.py
+++ pyheaders/cpp/scope.py
@@ -2,8 +2,10 @@
 Represents a C++ scope (namespace, class, enum class, ...).
 '''
 
+import re
+
 from collections import OrderedDict
-from typing import Any, Text, Optional, Tuple
+from typing import Any, Text, Optional, Pattern, Tuple
 
 
 class Scope(OrderedDict):
@@ -11,14 +13,45 @@ class Scope(OrderedDict):
     Represents a C++ scope (namespace, class, enum class, ...).
     '''
     SEP: Text = '::'
+    # Equivalent to: \([^:()]*\banonymous\b[^:()]*\)::
+    ANONYMOUS_NAMESPACE: Pattern = rf'\([^{SEP}()]*\banonymous\b[^{SEP}()]*\){SEP}'
+
+    _TEMPLATE_START = '<'
+    _TEMPLATE_END = '>'
+    _PLACEHOLDER = '@'
+    assert _PLACEHOLDER not in SEP
+
+    @staticmethod
+    def _safe_name(name: Text) -> Text:
+        '''
+        Prevent detection of scope separators inside templates
+        '''
+        safe_name = ''
+        template_level = 0
+        i = 0
+        while i < len(name):
+            if name[i] == Scope._TEMPLATE_START:
+                template_level += 1
+            elif name[i] == Scope._TEMPLATE_END:
+                template_level -= 1
+
+            if name.find(Scope.SEP, i, i + len(Scope.SEP)) == i and template_level:
+                safe_name += Scope._PLACEHOLDER * len(Scope.SEP)
+                i += len(Scope.SEP)
+            else:
+                safe_name += name[i]
+                i += 1
+
+        return safe_name
 
     @staticmethod
     def _extract_first_name(name: Text) -> Tuple[Text, Optional[Text]]:
-        first_sep_index = name.find(Scope.SEP)
+        name = normalize(name)
+
+        first_sep_index = Scope._safe_name(name).find(Scope.SEP)
         if first_sep_index >= 0:
             return name[:first_sep_index], name[first_sep_index + len(Scope.SEP):]
-        else:
-            return name, None
+        return name, None
 
     def __getitem__(self, name: Text):
         if not isinstance(name, str):
@@ -28,8 +61,7 @@ class Scope(OrderedDict):
 
         if inner is not None:
             return super().__getitem__(name)[inner]
-        else:
-            return super().__getitem__(name)
+        return super().__getitem__(name)
 
     def __setitem__(self, name: Text, value: Any):
         if not isinstance(name, str):
@@ -55,13 +87,25 @@ class Scope(OrderedDict):
         name, inner = Scope._extract_first_name(name)
         if inner is not None:
             return super().__contains__(name) and inner in self[name]
-        else:
-            return super().__contains__(name)
+        return super().__contains__(name)
+
+
+def normalize(name: Text) -> Text:
+    '''
+    Normalize name by removing leading namespace separators and anonymous namespaces.
+    '''
+    if name.startswith(Scope.SEP):
+        name = name[len(Scope.SEP):]
+
+    return re.sub(Scope.ANONYMOUS_NAMESPACE, '', name)
 
 
 def split(name: Text) -> Tuple[Optional[Text], Text]:
-    first_sep_index = name.rfind(Scope.SEP)
+    '''
+    Separate the last name component from the "scope" part.
+    '''
+    # pylint: disable=protected-access
+    first_sep_index = Scope._safe_name(name).rfind(Scope.SEP)
     if first_sep_index >= 0:
         return name[:first_sep_index], name[first_sep_index + len(Scope.SEP):]
-    else:
-        return None, name
+    return None, name
