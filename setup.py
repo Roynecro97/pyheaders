2a66ff1bde0bc7ee5ad5908c2ba44341bcbba96a
Swey9 <18035997+Swey9@users.noreply.github.com>
2020-05-25 11:42:29 +0300

    Updated version to 0.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if os.path.exists(README_PATH):
 
 setuptools.setup(
     name='pyheaders',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/Roynecro97/pyheaders',
     project_urls={
         "Source Code": 'https://github.com/Roynecro97/pyheaders'
